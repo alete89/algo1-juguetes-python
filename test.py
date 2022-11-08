bc0b6dfbf4b33e1756803f53cb8fdf8cb48081ea
alete89 <dini.alejandro@gmail.com>
2022-11-08 19:43:22 -0300

    fix method names

--- test.py
+++ test.py
@@ -13,54 +13,54 @@ class TestJuguetes:
     zoe = Curiose(edad_en_meses=20, juguetes=[pelota, baldecito])
     milena = Revoltose(edad_en_meses=15)
 
-    def costo_fabricacion_pelota(self):
+    def test_costo_fabricacion_pelota(self):
         assert self.pelota.costo_fabricacion() == 24
 
-    def costo_fabricacion_baldecito(self):
+    def test_costo_fabricacion_baldecito(self):
         assert self.baldecito.costo_fabricacion() == 21
 
-    def costo_fabricacion_tachito(self):
+    def test_costo_fabricacion_tachito(self):
         assert self.tachito.costo_fabricacion() == 39
 
-    def eficacia_pelota(self):
+    def test_eficacia_pelota(self):
         assert self.pelota.eficacia() == 12
 
-    def eficacia_baldecito(self):
+    def test_eficacia_baldecito(self):
         assert self.baldecito.eficacia() == 6
 
-    def eficacia_tachito(self):
+    def test_eficacia_tachito(self):
         assert self.tachito.eficacia() == 15
 
-    def precio_pelota(self):
+    def test_precio_pelota(self):
         assert self.pelota.precio_venta() == 144
 
-    def precio_baldecito(self):
+    def test_precio_baldecito(self):
         assert self.baldecito.precio_venta() == 101
 
-    def precio_tachito(self):
+    def test_precio_tachito(self):
         assert self.tachito.precio_venta() == 189
 
-    def tiempo_valentin_pelota(self):
+    def test_tiempo_valentin_pelota(self):
         assert round(self.valentin.tiempo_de_entretenimiento(self.pelota), 2) == 13.2
 
-    def tiempo_zoe_pelota(self):
+    def test_tiempo_zoe_pelota(self):
         assert round(self.zoe.tiempo_de_entretenimiento(self.pelota), 2) == 21.6
 
-    def tiempo_milena_pelota(self):
+    def test_tiempo_milena_pelota(self):
         assert round(self.milena.tiempo_de_entretenimiento(self.pelota), 2) == 6
 
-    def valentin_acepta_todo(self):
+    def test_valentin_acepta_todo(self):
         assert all(self.valentin.acepta_juguete(juguete) for juguete in self.todos_los_juguetes)
 
-    def zoe_acepta_pelota_y_baldecito(self):
+    def test_zoe_acepta_pelota_y_baldecito(self):
         assert [self.pelota, self.baldecito] == list(
             filter(lambda juguete: self.zoe.acepta_juguete(juguete), self.todos_los_juguetes))
 
-    def milena_acepta_solo_tachito(self):
+    def test_milena_acepta_solo_tachito(self):
         assert [self.tachito] == list(
             filter(lambda juguete: self.milena.acepta_juguete(juguete), self.todos_los_juguetes))
 
-    def valentin_dona_a_milena(self):
+    def test_valentin_dona_a_milena(self):
         self.valentin.donar_juguetes(self.milena)
         assert self.valentin.juguetes == [self.pelota, self.baldecito]
         assert self.milena.juguetes == [self.tachito]
